42e5bdb14691ed11b643cb4befb84a484013064c
Oleksandr Bavdyk <sbavdik@gmail.com>
2023-06-12 16:22:34 +0300

    attempt #4

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("dotenv/config");
-const phones_1 = require("./Controllers/phones");
+const phones_1 = require("./controllers/phones");
 const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.get('/phones', phones_1.getAllPhones);
