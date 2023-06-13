e590a9435a8f46cdfd63c52b8229bb833a83da79
Oleksandr Bavdyk <sbavdik@gmail.com>
2023-06-13 15:26:23 +0300

    installed cors

--- dist/app.js
+++ dist/app.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("dotenv/config");
-const phones_1 = require("./\u0421ontrollers/phones");
+const phones_1 = require("./controllers/phones");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+app.use((0, cors_1.default)());
 app.get('/phones', phones_1.getAllPhones);
 app.get('/phones/:phoneId', phones_1.getPhoneById);
 app.listen(port, () => {
