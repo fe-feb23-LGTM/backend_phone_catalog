4b5504ddf9dd10b6069ab2c06d2995060354f8fd
Andrii <remote.makandriival@gmail.com>
2023-06-13 14:35:25 +0300

    corsFix

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import 'dotenv/config'
 import { getAllPhones, getPhoneById } from './controllers/phones';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors())
 
 app.get('/phones', getAllPhones);
 
